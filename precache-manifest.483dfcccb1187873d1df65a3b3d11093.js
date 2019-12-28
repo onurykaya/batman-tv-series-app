e0ff17138904ea81ff2b0da7e4b94a98c3bc78ef
Onur Yerlikaya <onur.yerlikaya1@gmail.com>
2019-12-28 18:08:08 +0300

    Updates

---
+++ precache-manifest.483dfcccb1187873d1df65a3b3d11093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35db4fc7f814a9c0c1fd597f05b87177",
+    "url": "/batman-tv-series-app/index.html"
+  },
+  {
+    "revision": "d79af2993318f9d906a9",
+    "url": "/batman-tv-series-app/static/css/main.3fc576e0.chunk.css"
+  },
+  {
+    "revision": "3e2ec8c11e74db52dfb5",
+    "url": "/batman-tv-series-app/static/js/2.b2454a83.chunk.js"
+  },
+  {
+    "revision": "f8d07a0791b71cec298a1ad1be623170",
+    "url": "/batman-tv-series-app/static/js/2.b2454a83.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d79af2993318f9d906a9",
+    "url": "/batman-tv-series-app/static/js/main.3d601c0b.chunk.js"
+  },
+  {
+    "revision": "e76703dbbc8c8e4457c4",
+    "url": "/batman-tv-series-app/static/js/runtime-main.189b2942.js"
+  }
+]);
